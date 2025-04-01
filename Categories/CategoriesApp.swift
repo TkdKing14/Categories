6ef991b02b58240d0ec575aae34e00c2ab4714bc
Carson Payne <carson.j.payne14@gmail.com>
2025-04-01 09:51:49 -0500

    Initial Commit

---
+++ Categories/CategoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesApp.swift
+//  Categories
+//
+//  Created by Carson Payne on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CategoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
